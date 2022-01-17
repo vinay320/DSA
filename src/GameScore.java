b8f877af3addd3fb5370206ed90d50eda7852784
vinay320 <85616775+vinay320@users.noreply.github.com>
2022-01-17 15:01:31 +0530

    Update GameScore.java
    code completed but i used an extra array...

--- src/GameScore.java
+++ src/GameScore.java
@@ -18,23 +18,21 @@ public class GameScore {
         for (int i = 0; i < 5 ; i++) {
             arr[i]=new GameScore(s.next(),s.nextInt());
         }
+        int arr1[]=new int[5];
 
         for (int i = 0; i <5 ; i++) {
-            for (int j = 0; j <4 ; j++) {
-                if(arr[j].score<arr[j+1].score)
-                {
-                    GameScore temp = arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j]=temp;
-                }
-
-            }
-
+            arr1[i]=arr[i].score;
         }
+        Arrays.sort(arr1);
 
-        for (int i = 0; i <5 ; i++) {
-            System.out.println(arr[i].name+" "+arr[i].score);
-
+        for (int i = 0; i <5 ; i++)
+        {   int am=arr1[i];
+            for (int j=0;j<5;j++){
+            if(arr[j].score==am)
+                {
+                    System.out.println(arr[j].name + " " + arr[j].score);
+                }
+            }
         }
 
     }
